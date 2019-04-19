3404d83a6c69c8e0a5a4557d7aa16b41c2cf5c95
lc9900 <whynot_hm@hotmail.com>
2019-04-19 00:40:13 -0400

    more phrases added

--- phrases.js
+++ phrases.js
@@ -3,6 +3,13 @@ var compliments = [
     'You are such a joy',
     'You are the best swgoh player ever',
     "How can we ever live without you",
+    "Your focus determines your reality",
+    "Do. Or do not. There is no try",
+    "The Force will be with you. Always.",
+    "Do. Or do not. There is no try.",
+    "The force is strong with this one",
+
+
 ];
 
 var dis = [
@@ -10,6 +17,21 @@ var dis = [
     "Shove it where the sun don't shine",
     "You will feel the power of the dark side",
     "Tell it to someone who cares",
+    "Somebody has to save our skins.",
+    "I find your lack of faith disturbing",
+    "Help me! You’re my only hope",
+    "Why, you stuck-up, half-witted, scruffy-looking nerf herder!",
+    "No. I am your father",
+    "Fear is the path to the dark side. Fear leads to anger; anger leads to hate; hate leads to suffering. I sense much fear in you",
+    "Power! Unlimited power!",
+    "Judge me by my size, do you?",
+    "Traveling through hyperspace ain’t like dusting crops, farm boy",
+    "Fear is the path to the dark side.",
+    "I sense great fear in you. You have hate. You have anger. But you don’t use them.",
+    "Aren’t you a little short for a stormtrooper?",
+    "Into the garbage chute, fly boy!",
+    "Be careful not to choke on your aspirations.",
+
 ];
 
 var money = [
