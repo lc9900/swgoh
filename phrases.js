039c994205eba7dca5d07b0f30ad15aeeca2f401
lc9900 <whynot_hm@hotmail.com>
2019-04-19 01:05:50 -0400

    more phrase

--- phrases.js
+++ phrases.js
@@ -8,7 +8,7 @@ var compliments = [
     "The Force will be with you. Always.",
     "Do. Or do not. There is no try.",
     "The force is strong with this one",
-
+    "Help me! You’re my only hope",
 
 ];
 
@@ -17,21 +17,31 @@ var dis = [
     "Shove it where the sun don't shine",
     "You will feel the power of the dark side",
     "Tell it to someone who cares",
-    "Somebody has to save our skins.",
+    "If laughter is the best medicine, your face must be curing the world",
     "I find your lack of faith disturbing",
-    "Help me! You’re my only hope",
     "Why, you stuck-up, half-witted, scruffy-looking nerf herder!",
     "No. I am your father",
     "Fear is the path to the dark side. Fear leads to anger; anger leads to hate; hate leads to suffering. I sense much fear in you",
-    "Power! Unlimited power!",
     "Judge me by my size, do you?",
     "Traveling through hyperspace ain’t like dusting crops, farm boy",
     "Fear is the path to the dark side.",
     "I sense great fear in you. You have hate. You have anger. But you don’t use them.",
     "Aren’t you a little short for a stormtrooper?",
     "Into the garbage chute, fly boy!",
-    "Be careful not to choke on your aspirations.",
-
+    "You're so ugly, you scared the crap out of the toilet",
+    "No I'm not insulting you, I'm describing you",
+    "If I had a face like yours, I'd sue my parents",
+    "I guess you prove that even God makes mistakes sometimes",
+    "Your birth certificate is an apology letter from the doctor",
+    "If I want to kill myself, I'd climb your ego and jump to your IQ",
+    "Brains aren't everything. In your case, they are nothing",
+    "I don't know what makes you such a dbag, but it really works",
+    "I can explain it to you, but I can't understand it for you",
+    "Some babies were dropped on their heads, but you sir, were clearly thrown at a wall",
+    "Why don't you go play in traffic",
+    "Please shut your mouth when you talk to me",
+    "I'd slap you, but that would be animal abuse",
+    "The last time I saw something like you, I flushed it",
 ];
 
 var money = [
